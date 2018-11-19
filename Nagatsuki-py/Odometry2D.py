be6cef8f261ed7d8f6bee2601fec9ecf204c5175
Panepo <whisper.ko@gmail.com>
2018-11-19 14:12:42 +0800

    update notebook
    update notebook

--- Nagatsuki-py/Odometry2D.py
+++ Nagatsuki-py/Odometry2D.py
@@ -4,6 +4,11 @@ import cv2
 STAGE_FIRST_FRAME = 0
 STAGE_SECOND_FRAME = 1
 STAGE_DEFAULT_FRAME = 2
+
+SCALE_FIXED = 0
+SCALE_DISTANCE = 1
+SCALE_TIME = 2
+
 kMinNumFeature = 1500
 
 lk_params = dict(winSize = (21,21), maxLevel = 3, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
@@ -39,22 +44,37 @@ class VisualOdometry:
     self.prob = 0.999
     self.thre = 1.0
     self.scale = 1.0
+    if (self.dataset == SCALE_FIXED):
+      print("Operation mode: SCALE_FIXED")
+    elif (self.dataset == SCALE_DISTANCE):
+      print("Operation mode: SCALE_DISTANCE")
+    elif (self.dataset == SCALE_TIME):
+      print("Operation mode: SCALE_DISTANCE")
 
-  def readAnnotations(self, annotations):
-    with open(annotations) as f:
-      self.annotations = np.genfromtxt(f, delimiter=' ',dtype=None)
-      self.annoLength = len(self.annotations)
+  def readPosition(self, positions):
+    with open(positions) as f:
+      self.positions = np.genfromtxt(f, delimiter=' ',dtype=None)
+      self.posLength = len(self.positions)
+
+  def readTime(self, times):
+    with open(times) as f:
+      self.times = np.genfromtxt(f, delimiter=(12), dtype=None)
+      self.timesLength = len(self.times)
 
   def setParameter(self, prob, thre, scale):
     self.prob = prob
     self.thre = thre
     self.scale = scale
 
-  def getAbsoluteScale(self, frame_id):  #specialized for KITTI odometry dataset
-    x_pre, y_pre, z_pre = self.annotations[frame_id-1][3], self.annotations[frame_id-1][7], self.annotations[frame_id-1][11]
-    x, y, z = self.annotations[frame_id][3], self.annotations[frame_id][7], self.annotations[frame_id][11]
+  def getScalePos(self, frame_id): #specialized for KITTI odometry dataset
+    x_pre, y_pre, z_pre = self.positions[frame_id-1][3], self.positions[frame_id-1][7], self.positions[frame_id-1][11]
+    x, y, z = self.positions[frame_id][3], self.positions[frame_id][7], self.positions[frame_id][11]
     self.trueX, self.trueY, self.trueZ = x, y, z
     return np.sqrt((x - x_pre)*(x - x_pre) + (y - y_pre)*(y - y_pre) + (z - z_pre)*(z - z_pre))
+
+  def getScaleTime(self, frame_id): #specialized for KITTI odometry dataset
+    self.trueX, self.trueY, self.trueZ = self.positions[frame_id][3], self.positions[frame_id][7], self.positions[frame_id][11]
+    return (self.times[frame_id] - self.times[frame_id-1]) * 5
 
   def processFirstFrame(self):
     self.px_ref = self.detector.detect(self.new_frame)
@@ -77,14 +97,19 @@ class VisualOdometry:
     E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC, prob=self.prob, threshold=self.thre)
     _, R, t, mask = cv2.recoverPose(E, self.px_cur, self.px_ref, focal=self.focal, pp = self.pp)
 
-    if (self.dataset):
-      self.scale = self.getAbsoluteScale(frame_id)
+    if (self.dataset == SCALE_FIXED):
+      self.cur_t = self.cur_t + self.scale * self.cur_R.dot(t)
+      self.cur_R = R.dot(self.cur_R)
+    elif (self.dataset == SCALE_DISTANCE):
+      self.scale = self.getScalePos(frame_id)
       if(self.scale > 0.1):
         self.cur_t = self.cur_t + self.scale * self.cur_R.dot(t)
         self.cur_R = R.dot(self.cur_R)
-    else:
-      self.cur_t = self.cur_t + self.scale * self.cur_R.dot(t)
-      self.cur_R = R.dot(self.cur_R)
+    elif (self.dataset == SCALE_TIME):
+      self.scale = self.getScaleTime(frame_id)
+      if(self.scale > 0.1):
+        self.cur_t = self.cur_t + self.scale * self.cur_R.dot(t)
+        self.cur_R = R.dot(self.cur_R)
 
     self.px_ref = self.px_cur
 
